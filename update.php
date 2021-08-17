c563ff111698f94ecb8c4ee24b24011b47e112e9
Thani Ath Nain Khurshid <71865085+thani-ath-nain@users.noreply.github.com>
2021-08-17 10:10:18 +0500

    FIXED

--- update.php
+++ update.php
@@ -1,39 +1,53 @@
 <?php
 // Include config file
 require_once "config.php";
-
 // Define variables and initialize with empty values
 $name = "";
 $name_err = "";
-
 // Processing form data when form is submitted
 if (isset($_POST["id"]) && !empty($_POST["id"])) {
     // Get hidden input value
     $id = $_POST["id"];
-
     // Validate name
     $input_name = trim($_POST["name"]);
     if (empty($input_name)) {
         $name_err = "Please enter a name.";
-    } elseif (!filter_var($input_name, FILTER_VALIDATE_REGEXP, array("options" => array("regexp" => "/^[a-zA-Z\s]+$/")))) {
-        $name_err = "Please enter a valid name.";
+    } elseif (!preg_match('/^[a-zA-Z0-9_]+$/', trim($input_name))) {
+        $username_err = "Username can only contain letters, numbers, and underscores.";
     } else {
         $name = $input_name;
     }
 
+    // // Validate address address
+    // $input_address = trim($_POST["address"]);
+    // if (empty($input_address)) {
+    //     $address_err = "Please enter an address.";
+    // } else {
+    //     $address = $input_address;
+    // }
+
+    // // Validate salary
+    // $input_salary = trim($_POST["salary"]);
+    // if (empty($input_salary)) {
+    //     $salary_err = "Please enter the salary amount.";
+    // } elseif (!ctype_digit($input_salary)) {
+    //     $salary_err = "Please enter a positive integer value.";
+    // } else {
+    //     $salary = $input_salary;
+    // }
 
     // Check input errors before inserting in database
     if (empty($name_err)) {
         // Prepare an update statement
-        $sql = "UPDATE employees SET username=? WHERE id=?";
-
+        $sql = "UPDATE users SET username=? WHERE id=?";
         if ($stmt = $mysqli->prepare($sql)) {
             // Bind variables to the prepared statement as parameters
             $stmt->bind_param("si", $param_name, $param_id);
-
             // Set parameters
             $param_id = $id;
-            $param_name = $username;
+            $param_name = $name;
+            // $param_address = $address;
+            // $param_salary = $salary;
 
 
             // Attempt to execute the prepared statement
@@ -45,11 +59,9 @@ if (isset($_POST["id"]) && !empty($_POST["id"])) {
                 echo "Oops! Something went wrong. Please try again later.";
             }
         }
-
         // Close statement
         $stmt->close();
     }
-
     // Close connection
     $mysqli->close();
 } else {
@@ -57,20 +69,16 @@ if (isset($_POST["id"]) && !empty($_POST["id"])) {
     if (isset($_GET["id"]) && !empty(trim($_GET["id"]))) {
         // Get URL parameter
         $id =  trim($_GET["id"]);
-
         // Prepare a select statement
         $sql = "SELECT * FROM users WHERE id = ?";
         if ($stmt = $mysqli->prepare($sql)) {
             // Bind variables to the prepared statement as parameters
             $stmt->bind_param("i", $param_id);
-
             // Set parameters
             $param_id = $id;
-
             // Attempt to execute the prepared statement
             if ($stmt->execute()) {
                 $result = $stmt->get_result();
-
                 if ($result->num_rows == 1) {
                     /* Fetch result row as an associative array. Since the result set
                     contains only one row, we don't need to use while loop */
@@ -78,6 +86,8 @@ if (isset($_POST["id"]) && !empty($_POST["id"])) {
 
                     // Retrieve individual field value
                     $name = $row["username"];
+                    // $address = $row["address"];
+                    // $salary = $row["salary"];
                 } else {
                     // URL doesn't contain valid id. Redirect to error page
                     header("location: error.php");
@@ -87,10 +97,8 @@ if (isset($_POST["id"]) && !empty($_POST["id"])) {
                 echo "Oops! Something went wrong. Please try again later.";
             }
         }
-
         // Close statement
         $stmt->close();
-
         // Close connection
         $mysqli->close();
     } else {
@@ -100,10 +108,8 @@ if (isset($_POST["id"]) && !empty($_POST["id"])) {
     }
 }
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <title>Update Record</title>
@@ -115,7 +121,6 @@ if (isset($_POST["id"]) && !empty($_POST["id"])) {
         }
     </style>
 </head>
-
 <body>
     <div class="wrapper">
         <div class="container-fluid">
@@ -129,7 +134,17 @@ if (isset($_POST["id"]) && !empty($_POST["id"])) {
                             <input type="text" name="name" class="form-control <?php echo (!empty($name_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $name; ?>">
                             <span class="invalid-feedback"><?php echo $name_err; ?></span>
                         </div>
-                        <input type="hidden" name="id" value="<?php echo $id; ?>" />
+                        <!-- <div class="form-group">
+                            <label>Address</label>
+                            <textarea name="address" class="form-control <?php echo (!empty($address_err)) ? 'is-invalid' : ''; ?>"><?php echo $address; ?></textarea>
+                            <span class="invalid-feedback"><?php echo $address_err; ?></span>
+                        </div>
+                        <div class="form-group">
+                            <label>Salary</label>
+                            <input type="text" name="salary" class="form-control <?php echo (!empty($salary_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $salary; ?>">
+                            <span class="invalid-feedback"><?php echo $salary_err; ?></span>
+                        </div> -->
+                        <input type="hidden"  name="id" value="<?php echo $id; ?>" />
                         <input type="submit" class="btn btn-primary" value="Submit">
                         <a href="crud.php" class="btn btn-secondary ml-2">Cancel</a>
                     </form>
@@ -138,5 +153,4 @@ if (isset($_POST["id"]) && !empty($_POST["id"])) {
         </div>
     </div>
 </body>
-
 </html>
